feat add bounding according to volume size; add percentile normalization; fix generateData for zoomed CT (2view)
Signed-off-by: bnbqq8 <bnbqq8@i.smu.edu.cn>

dataGenerator/generateData.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 import os.path as osp
 import pickle
 from pathlib import Path
@@ -17,38 +17,41 @@ import yaml
 from tigre.utilities import CTnoise, gpu
 from tigre.utilities.geometry import Geometry
 
-# def config_parser():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--ctName", default="chest", type=str,
-#                         help="Name of CT")
-#     parser.add_argument("--outputName", default="chest_50", type=str,
-#                         help="Name of output data")
-#     parser.add_argument("--dataFolder", default="raw", type=str,
-#                         help="folder of raw data")
-#     parser.add_argument("--outputFolder", default="./data", type=str,
-#                         help="folder of output data")
-#     return parser
+
+def config_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--configPath",
+        default="./dataGenerator/config_2views.yml",
+        type=str,
+        help="Path of config file",
+    )
+    parser.add_argument(
+        "--dataPath",
+        default="/home/public/CTSpine1K/data/data-MHD_ctpro_woMask1",
+        type=str,
+        help="Path of output data",
+    )
+    parser.add_argument(
+        "--case",
+        default="volume-covid19-A-0377_ct",
+        type=str,
+        help="Path of case folder, which should contain ct_file.mha",
+    )
+
+    parser.add_argument(
+        "--outputName", default="data_2views", type=str, help="Name of output data"
+    )
+    return parser
 
 
 def main():
-    # parser = config_parser()
-    # args = parser.parse_args()
-    # dataType = args.ctName
-    # dataFolder = args.dataFolder
-    # outputName = args.outputName
-    # outputFolder = args.outputFolder
-    configPath = f"dataGenerator/config_IXI2views.yml"
-    # for case in Path("/home/public/CTSpine1K/data/data-MHD_ctpro_woMask1").glob("*"):
-    #     if case.stem != "liver_6":
-    #         continue
-    case = Path("/home/czfy/IXI_dataset/IXI_downsampledx4_iacl_SyN/IXI075-Guys-0754/")
-    # matPath = str(case / "ct_file.mha")
-    # outputPath = str(case / "data_2views.pickle")
-    matPath = str(case / "T1_gt.nii.gz")
-    outputPath = str(case / "data_2views.pickle")
-    # matPath = f"./dataGenerator/{dataFolder}/{dataType}/img.mat"
-    # configPath = f"./dataGenerator/{dataFolder}/{dataType}/config.yml"
-    # outputPath = osp.join(outputFolder, f"{outputName}.pickle")
+    parser = config_parser()
+    args = parser.parse_args()
+    configPath = Path(args.configPath)
+    case = Path(args.dataPath) / Path(args.case)
+    matPath = str(case / "ct_file.mha")
+    outputPath = case / f"{args.outputName}.pickle"
     generator(matPath, configPath, outputPath, True)
 
 
@@ -133,18 +136,20 @@ def convert_to_attenuation(
 
 
 def loadImage(
-    dirname, nVoxels, convert, rescale_slope, rescale_intercept, normalize=True
+    test_data,
+    zoom_factor,
+    nVoxels,
+    convert,
+    rescale_slope,
+    rescale_intercept,
+    normalize=True,
+    percentile=False,
+    min=None,
+    max=None,
 ):
     """
     Load CT image.
     """
-
-    if nVoxels is None:
-        nVoxels = np.array((256, 256, 256))
-
-    # test_data = scipy.io.loadmat(dirname)
-    test_data = sitk.GetArrayFromImage(sitk.ReadImage(dirname))
-    test_data = np.transpose(test_data, (2, 1, 0)).astype(np.float32)
 
     # Loads data in F_CONTIGUOUS MODE (column major), convert to Row major
     image_ori = test_data
@@ -156,17 +161,13 @@ def loadImage(
 
     imageDim = image.shape
 
-    zoom_x = nVoxels[0] / imageDim[0]
-    zoom_y = nVoxels[1] / imageDim[1]
-    zoom_z = nVoxels[2] / imageDim[2]
-
-    if zoom_x != 1.0 or zoom_y != 1.0 or zoom_z != 1.0:
+    if zoom_factor != 1.0:
         print(
             f"Resize ct image from {imageDim[0]}x{imageDim[1]}x{imageDim[2]} to "
             f"{nVoxels[0]}x{nVoxels[1]}x{nVoxels[2]}"
         )
-        image = scipy.ndimage.interpolation.zoom(
-            image, (zoom_x, zoom_y, zoom_z), order=3, prefilter=False
+        image = scipy.ndimage.zoom(
+            image, (zoom_factor, zoom_factor, zoom_factor), order=3, prefilter=False
         )
 
     image_max = np.max(image)
@@ -177,9 +178,36 @@ def loadImage(
     )
     if normalize and image_min != 0 and image_max != 1:
         print("Normalize range to [0, 1]")
+        if percentile:
+            p1 = np.percentile(image, 0.05)
+            p99 = np.percentile(image, 99.9)
+            image = np.clip(image, p1, None)
+            image_min = p1
+            image_max = p99
+        else:
+            image_min = min if min is not None else image_min
+            image_max = max if max is not None else image_max
+            image = np.clip(image, image_min, None)
         image = (image - image_min) / (image_max - image_min)
 
     return image
+
+
+def calc_nDetector(DSD, DSO, nVoxel, dVoxel, dDetector):
+    """
+    Calculate number of detector pixels using similar similar triangles:
+    """
+    # nDetector_W/nVoxel_W = DSD/(DSO-nVoxel_W*dVoxel_W/2)
+    nVoxel_W = nVoxel[0]
+    nDetector_W = np.round(nVoxel_W * DSD / (DSO - nVoxel_W * dVoxel[0] / 2)).astype(
+        int
+    )
+    # nDetector_H/nVoxel_H = DSD/(DSO-nVoxel_H*dVoxel/2)
+    nVoxel_H = nVoxel[-1]
+    nDetector_H = np.round(nVoxel_H * DSD / (DSO - nVoxel_H * dVoxel[-1] / 2)).astype(
+        int
+    )
+    return [nDetector_W, nDetector_H]
 
 
 def generator(matPath, configPath, outputPath, show=True):
@@ -191,23 +219,41 @@ def generator(matPath, configPath, outputPath, show=True):
     # Load configuration
     with open(configPath, "r") as handle:
         data = yaml.safe_load(handle)
+    # load ct image
+    test_data = sitk.GetArrayFromImage(sitk.ReadImage(matPath))  # z,y,x
+    test_data = np.transpose(test_data, (2, 1, 0)).astype(np.float32)  # x,y,z
+    # overwrite nVoxel
+    target_img_size = 256
+    zoom_factor = target_img_size / test_data.shape[0]
+    data["nVoxel"] = [
+        target_img_size,
+        target_img_size,
+        round(test_data.shape[2] * zoom_factor),
+    ]
 
-    # Load CT image
-    geo = ConeGeometry_special(data)
+    # proprocessing
     img = loadImage(
-        matPath,
+        test_data,
+        zoom_factor,
         data["nVoxel"],
         data["convert"],
         data["rescale_slope"],
         data["rescale_intercept"],
         data["normalize"],
+        data["percentile"],
+        data["min"],
+        data["max"],
     )
     data["image"] = img.copy()
+    # calc nDetector
+    data["nDetector"] = calc_nDetector(
+        data["DSD"], data["DSO"], data["nVoxel"], data["dVoxel"], data["dDetector"]
+    )
 
     # plt.figure()
     # plt.imshow(img[:,:,0])
     # plt.show()
-
+    geo = ConeGeometry_special(data)
     # Generate training images
     if data["randomAngle"] is False:
         data["train"] = {
@@ -242,7 +288,7 @@ def generator(matPath, configPath, outputPath, show=True):
         data["train"]["projections"] = projections
 
     # Generate validation images
-    if "2view" in configPath:
+    if "2view" in configPath.name:
         data["val"] = {
             "angles": np.linspace(
                 0, data["totalAngle"] / 180 * np.pi, data["numTrain"] + 1
@@ -279,15 +325,15 @@ def generator(matPath, configPath, outputPath, show=True):
         # save to tmp/
         os.makedirs("tmp", exist_ok=True)
         tigre.plotimg(
-            img.transpose((2, 0, 1)), dim="z", savegif="tmp/IXI_image_2views.gif"
+            img.transpose((2, 0, 1)), dim="z", savegif=outputPath.parent / f"image.gif"
         )
         tigre.plotproj(
             data["train"]["projections"][:, ::-1, :],
-            savegif="tmp/IXItrain_projections_2views.gif",
+            savegif=outputPath.parent / f"train_projections.gif",
         )
         tigre.plotproj(
             data["val"]["projections"][:, ::-1, :],
-            savegif="tmp/IXIval_projections_2views.gif",
+            savegif=outputPath.parent / f"val_projections.gif",
         )
 
     # Save data

src/dataset/tigre.py
@@ -1,9 +1,9 @@
-import torch
-import pickle
 import os
+import pickle
 import sys
-import numpy as np
 
+import numpy as np
+import torch
 from torch.utils.data import DataLoader, Dataset
 
 
@@ -66,7 +66,7 @@ class TIGREDataset(Dataset):
         self.geo = ConeGeometry(data)
         self.type = type
         self.n_rays = n_rays
-        self.near, self.far = self.get_near_far(self.geo)
+        self.near, self.far = self.get_near_far(self.geo, tolerance=0.0)
 
         if type == "train":
             self.projs = torch.tensor(
@@ -103,9 +103,9 @@ class TIGREDataset(Dataset):
             )
             self.coords = torch.reshape(coords, [-1, 2])
             self.image = torch.tensor(data["image"], dtype=torch.float32, device=device)
-            self.voxels = torch.tensor(
-                self.get_voxels(self.geo), dtype=torch.float32, device=device
-            )
+            # self.voxels = torch.tensor(
+            #     self.get_voxels(self.geo), dtype=torch.float32, device=device
+            # )
         elif type == "val":
             self.projs = torch.tensor(
                 data["val"]["projections"], dtype=torch.float32, device=device

src/render/render.py
@@ -2,7 +2,26 @@ import torch
 import torch.nn as nn
 
 
-def render(rays, net, net_fine, n_samples, n_fine, perturb, netchunk, raw_noise_std):
+def bounding_points(pts, bound_box):
+    bx, by, bz = bound_box
+    # 1. 计算 Mask：哪些点在物理长方体内
+    in_bound_mask = (pts[..., 0] >= -bx) & (pts[..., 0] <= bx) & \
+                    (pts[..., 1] >= -by) & (pts[..., 1] <= by) & \
+                    (pts[..., 2] >= -bz) & (pts[..., 2] <= bz)
+    
+    # 2. 生成安全的查询坐标 (Clamp 防止越界)
+    pts_safe = pts.clone()
+    pts_safe[..., 0] = pts_safe[..., 0].clamp(-bx, bx)
+    pts_safe[..., 1] = pts_safe[..., 1].clamp(-by, by)
+    pts_safe[..., 2] = pts_safe[..., 2].clamp(-bz, bz)
+    
+    # ==================== 新增：坐标归一化 ====================
+    # 除以长方体边界，映射到 HashGrid 默认的 [-1, 1]
+    bound_tensor = torch.tensor([bx, by, bz], device=pts.device)
+    pts = pts_safe / bound_tensor
+    # ========================================================
+    return pts, in_bound_mask
+def render(rays, net, net_fine, n_samples, n_fine, perturb, netchunk, raw_noise_std,bound_box=None):
     n_rays = rays.shape[0]
     rays_o, rays_d, near, far = rays[...,:3], rays[...,3:6], rays[...,6:7], rays[...,7:]
 
@@ -21,10 +40,14 @@ def render(rays, net, net_fine, n_samples, n_fine, perturb, netchunk, raw_noise_
         z_vals = lower + (upper - lower) * t_rand
 
     pts = rays_o[..., None, :] + rays_d[..., None, :] * z_vals[..., :, None]  # [n_rays, n_samples, 3]
-    bound = net.bound - 1e-6
-    pts = pts.clamp(-bound, bound)
-
+    if bound_box is None:
+        bound = net.bound - 1e-6
+        pts = pts.clamp(-bound, bound)
+    else:
+        pts, in_bound_mask = bounding_points(pts, bound_box)
     raw = run_network(pts, net, netchunk)
+    if bound_box is not None:
+        raw[..., 0][~in_bound_mask] = 0.0 # 物理边界外的点密度设为0
     acc, weights = raw2outputs(raw, z_vals, rays_d, raw_noise_std)
 
     if net_fine is not None and n_fine > 0:
@@ -38,8 +61,14 @@ def render(rays, net, net_fine, n_samples, n_fine, perturb, netchunk, raw_noise_
 
         z_vals, _ = torch.sort(torch.cat([z_vals, z_samples], -1), -1)
         pts = rays_o[..., None, :] + rays_d[..., None, :] * z_vals[..., :, None]
-        pts = pts.clamp(-bound, bound)
+        if bound_box is None:
+            bound = net_fine.bound - 1e-6
+            pts = pts.clamp(-bound, bound)
+        else:
+            pts, in_bound_mask = bounding_points(pts, bound_box)
         raw = run_network(pts, net_fine, netchunk)
+        if bound_box is not None:
+            raw[..., 0][~in_bound_mask] = 0.0 # 物理边界外的点密度设为0
         acc, _ = raw2outputs(raw, z_vals, rays_d, raw_noise_std)
 
     ret = {"acc": acc, "pts":pts}

src/trainer.py
@@ -38,6 +38,7 @@ class Trainer:
             spacing = cfg["train"]["sds"].get("spacing", 1.0)
             self.bound_z = self.img_dims[2] * spacing / 2.0 /1000.0 # mm to m
             self.bound_xy = self.img_dims[0] * spacing / 2.0 /1000.0 # mm to m
+            self.bound_box = torch.tensor([self.bound_xy, self.bound_xy, self.bound_z], device=device)
             self.device = device
             # prepare vesde
             ckpt_path = cfg["train"]["sds"].get("ckpt_path", None)
@@ -268,10 +269,15 @@ class Trainer:
         batch_z = z_vals.expand(B, res, res) # [B, H, W]
         
         # 堆叠坐标: [B, H, W, 3]
-        coords_3d = torch.stack([batch_y, batch_x, batch_z], dim=-1)
+        coords_3d = torch.stack([batch_x, batch_y, batch_z], dim=-1)
         
         # 4. 展平并查询网络
         coords_flat = coords_3d.reshape(-1, 3) # [B*H*W, 3]
+        
+        # normalizing points coords
+        bound_tensor = torch.tensor(self.bound_box, device=self.device)
+        coords_flat = coords_flat / bound_tensor # normalize to [-1, 1]
+        
         
         # 使用你原有的 run_network
         from src.render.render import run_network 
@@ -280,11 +286,11 @@ class Trainer:
         # 5. 重塑与归一化
         pred_slices = density_flat.reshape(B, 1, res, res)
         
-        # 归一化 (根据你的 mu_water 调整)
-        scale_factor = 1.0 / (self.mu_water * 1.2) if hasattr(self, 'mu_water') else 10.0
-        pred_slices_norm = pred_slices * scale_factor
+        # # 归一化 (根据你的 mu_water 调整)
+        # scale_factor = 1.0 / (self.mu_water * 1.2) if hasattr(self, 'mu_water') else 10.0
+        # pred_slices_norm = pred_slices * scale_factor
         
-        return pred_slices_norm
+        return pred_slices
     
     def compute_loss(self, data, global_step, idx_epoch):
         """

train.py
@@ -1,12 +1,14 @@
 import argparse
 import os
 import os.path as osp
+from math import e
 
 import imageio.v2 as iio
 import numpy as np
 import torch
+from zmq import has
 
-from src.config.configloading import load_config
+from src.config.configloading import load_config, save_cfg
 from src.loss import calc_mse_loss
 from src.render import render, run_network
 from src.trainer import Trainer
@@ -32,7 +34,7 @@ parser = config_parser()
 args = parser.parse_args()
 
 cfg = load_config(args.config)
-
+save_cfg(cfg)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -47,7 +49,13 @@ class BasicTrainer(Trainer):
     def compute_loss(self, data, global_step, idx_epoch):
         rays = data["rays"].reshape(-1, 8)
         projs = data["projs"].reshape(-1)
-        ret = render(rays, self.net, self.net_fine, **self.conf["render"])
+        ret = render(
+            rays,
+            self.net,
+            self.net_fine,
+            bound_box=self.bound_box,
+            **self.conf["render"],
+        )
         projs_pred = ret["acc"]
 
         loss = {"loss": 0.0}
@@ -75,6 +83,7 @@ class BasicTrainer(Trainer):
                     rays[i : i + self.n_rays],
                     self.net,
                     self.net_fine,
+                    bound_box=self.bound_box,
                     **self.conf["render"],
                 )["acc"]
             )
@@ -82,8 +91,15 @@ class BasicTrainer(Trainer):
 
         # Evaluate density
         image = self.eval_dset.image
+        if hasattr(self, "bound_box"):
+            bound_tensor = (
+                self.bound_box.clone().detach().to(self.eval_dset.voxels.device)
+            )
+            voxels = self.eval_dset.voxels / bound_tensor
+        else:
+            voxels = self.eval_dset.voxels
         image_pred = run_network(
-            self.eval_dset.voxels,
+            voxels,
             self.net_fine if self.net_fine is not None else self.net,
             self.netchunk,
         )